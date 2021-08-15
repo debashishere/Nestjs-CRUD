9041c3cbc1bd11e8c00f203ea9a3847739b93989
debashishere <debashishere007@gmail.com>
2021-08-15 13:59:26 +0530

    adding UserController unit tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -5,16 +5,63 @@ import { UsersService } from './users.service';
 describe('UsersController', () => {
   let controller: UsersController;
 
+  // mock services
+  const mockUserService = {
+
+    // simulation of database service
+    create: jest.fn(dto => {
+      return {
+        id: Date.now(),
+        ...dto,
+      }
+    }),
+    update: jest.fn((id, dto) => {
+      return {
+        id,
+        ...dto,
+      }
+    })
+  }
+
   beforeEach(async () => {
+
+    // simulation of insolated module for one test
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
-    }).compile();
+    })
+    // over write UserService dependency 
+    .overrideProvider(UsersService)
+    .useValue(mockUserService)
+    .compile();
 
+    // user controller
     controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a user', () => {
+    const dto = { name: 'Merius' };
+    expect(controller.create(dto)).toEqual({
+      id: expect.any(Number),
+      name: dto.name
+    })
+
+    expect(mockUserService.create).toHaveBeenCalledWith(dto);
+  })
+
+  it('should update a user', () => {
+    const dto = { name: "updated deba" }
+
+    expect(controller.update( '1', dto)).toEqual({
+      id: 1,
+      ...dto,
+    })
+
+    expect(mockUserService.update).toHaveBeenCalled();
+  })
+
 });
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity'
 
 @Module({
+  // injecting our  repository for a user's table in out database
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService]
